ae1d97f0dc73055ebe1d2f0d8eb8180084ab3faa
Yashodhan Joshi <yash.sumedha@gmail.com>
2020-04-13 19:11:14 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// const AESCryptor = require('./lib/crypto_classes/AESCryptor');
+
+// const aesc = new AESCryptor.AESCryptor();
+
+// const key = '$2a$04$Lf8s3jIlTbq5JMlcTIladOs3j';
+// // const encTxt = Buffer.from(
+// //   'd426b7add3f9a28a80a8704b2d131945e652b794bddc34d81a1d90868c415fa97e536c393c90cc33a32335aa2208044a67883971a9828c',
+// //   'hex'
+// // ).toString('base64');
+// //const ret = aesc.decrypt(key, encTxt, 5);
+
+// const obj = { server: 'A', Target: 'B' };
+// //const enc = aesc.encrypt(key, JSON.stringify(obj), 5);
+
+// //console.log(enc);
+// //const enc = '1CaWrdCt/drx9yIfCUMVJOYK9JKz2iufVB+I5uZZ';
+// const enc = '1CaWrdCt/drx9yIfCUMVJOYK9JKz2iufVB+I5+ZZ';
+// const dec = aesc.decrypt(key, enc, 5);
+// console.log(dec);
